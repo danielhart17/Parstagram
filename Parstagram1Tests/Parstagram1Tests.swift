8135002a851fdd143e06e742dcfb8af744033ef8
Daniel Hart <43150766+danielhart17@users.noreply.github.com>
2020-03-03 16:48:55 -0500

    Initial Commit

---
+++ Parstagram1Tests/Parstagram1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Parstagram1Tests.swift
+//  Parstagram1Tests
+//
+//  Created by Daniel Hart on 3/3/20.
+//  Copyright © 2020 Daniel Hart. All rights reserved.
+//
+
+import XCTest
+@testable import Parstagram1
+
+class Parstagram1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parstagram1' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Parstagram1
+  pod 'AlamofireImage'
+  pod 'Parse'
+  pod 'MessageInputBar'
+
+
+  target 'Parstagram1Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Parstagram1UITests' do
+    # Pods for testing
+  end
+
+end
